2ba272a4d2c326043c331761a15b015b752d1795
Alejandra Josue <ajosue92@gmail.com>
2016-10-17 15:27:12 -0600

    Dictamen juridico creado

---
+++ src/app/judicial/dictamen_juridico/dictamen_juri.js
@@ -0,0 +1,17 @@
+/**
+ * Created by alejandra on 16/10/16.
+ */
+(function() {
+  'use strict';
+
+  angular
+    .module('frontendJudicial')
+    .controller('DJ_Controller', DJ_Controller);
+
+  /** @ngInject */
+  function DJ_Controller() {
+    var vm = this;
+
+  }
+
+})();
