98b748dfa06c0650d055c109620221fcbedd9478
Alejandra Josue <ajosue92@gmail.com>
2016-10-17 15:18:02 -0600

    Dictamen Telefonia creado

---
+++ src/app/judicial/dictamen_tele/dictamen_tele.js
@@ -0,0 +1,17 @@
+/**
+ * Created by alejandra on 16/10/16.
+ */
+(function() {
+  'use strict';
+
+  angular
+    .module('frontendJudicial')
+    .controller('DT_Controller', DT_Controller);
+
+  /** @ngInject */
+  function DT_Controller() {
+    var vm = this;
+
+  }
+
+})();
